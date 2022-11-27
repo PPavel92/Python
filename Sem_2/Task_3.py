bfb99cb3feb0a1354b37891a206eaa23732c77dc
Pavel <Proskuryakovp@bk.ru>
2022-11-27 20:40:02 +0600

    HomeWork2

--- Sem_2/Task_3.py
+++ Sem_2/Task_3.py
@@ -4,15 +4,24 @@
 # 'лю'
 # --> 2
 
-text = 'Я люблю Python'
-char = input('Введите значение поиска: ')
-len_char = len(char)
-i = 0
-count = 0
-while i < len(text)-1:
-    if char.lower() == text[i:len_char+i].lower():
-        count += 1
-    i += 1
-print(count)
+# text = 'Я люблю Python'
+# char = input('Введите значение поиска: ')
+# len_char = len(char)
+# i = 0
+# count = 0
+# while i < len(text)-1:
+#     if char.lower() == text[i:len_char+i].lower():
+#         count += 1
+#     i += 1
+# print(count)
+
+
+
+# Через сплит.
+text = 'Я люблю Python люблю'
+searchText = input('Введите строку для подсчета вхождения: ')
+
+list = text.split(searchText)
+print(len(list)-1)
 
 
