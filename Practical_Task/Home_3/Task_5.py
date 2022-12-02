5d718b8aa40dd6f9988b3d9584d2264b8b562a27
Pavel <Proskuryakovp@bk.ru>
2022-12-02 12:11:31 +0600

    HomeWork3

--- Practical_Task/Home_3/Task_5.py
+++ Practical_Task/Home_3/Task_5.py
@@ -4,29 +4,27 @@
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-# n = int(input("Введите число: "))
-# fib1 = 1
-# fib2 = 0
+n = int(input("Введите число: "))
 
-# i = 0
-# while i < n :
-#     fib_sum = fib1 + fib2
-#     fib1 = fib2
-#     fib2 = fib_sum
-#     i += 1
-#     print(fib2)
-# list = []
-# data = list(fib2)
-# print(data)
-
-# не доделал
-
-def fibonacci(n):
-    a, b = 1, 1
+def fib_m(n):
+    a, b = 0, 1
+    result = [0]
     for i in range(n):
-        yield a
         a, b = b, a + b
-        
+        result.append(a*-1)
+    return result
 
-data = list(fibonacci(8))
-print(data)
+def fib_p(n):
+    a, b = 0, 1
+    result = []
+    for i in range(n):
+        a, b = b, a + b
+        result.append(a)
+    return result
+
+result1 = []
+result2 = []
+result1 = fib_m(n)
+result2 = fib_p(n)
+result1 = result1[::-1]
+print(result1 + result2)
